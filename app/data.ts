99ebb10c35c86b2822353fbfe5bda47d9d0e372e
DeveloperOffshore publishing agent <publishing-agent@developeroffshore.com>
2026-09-08 13:23:54 +0000

    Publish September 8 blog and research batches

--- app/data.ts
+++ app/data.ts
@@ -14,6 +14,7 @@ import { september2BlogBatch } from './sep02-blog-batch';
 import { september3BlogBatch } from './sep03-blog-batch';
 import { september4BlogBatch } from './sep04-blog-batch';
 import { september7BlogBatch } from './sep07-blog-batch';
+import { september8BlogBatch } from './sep08-blog-batch';
 
 export const site = {
   "domain": "DeveloperOffshore.com",
@@ -453,6 +454,7 @@ const dailyBlogBatch20260810Additional: BlogPost[] = additionalDailyTopics202608
 }));
 
 export const blogPosts: BlogPost[] = [
+  ...september8BlogBatch,
   ...september7BlogBatch,
   ...september4BlogBatch,
   ...september3BlogBatch,
